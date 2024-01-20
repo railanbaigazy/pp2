d8cc224bdb1d4a8f8c739a46b85aa8a73089e724
Railan Baigazy <69196082+railanbaigazy@users.noreply.github.com>
2024-01-20 16:43:34 +0600

    w3s half commit

---
+++ w3schools/booleans.py
@@ -0,0 +1,19 @@
+#1
+print(10 > 9)
+#output: True
+
+#2
+print(10 == 9)
+#output: False
+
+#3
+print(10 < 9)
+#output: False
+
+#4
+print(bool("abc"))
+#output: True
+
+#5
+print(bool(0))
+#output: False
---
+++ w3schools/data_types.py
@@ -0,0 +1,35 @@
+#1
+x = 5
+print(type(x))
+#output: int
+
+#2
+x = "Hello World"
+print(type(x))
+#output: str
+
+#3
+x = 20.5
+print(type(x))
+#output: float
+
+#4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#output: list
+
+#5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#output: tuple
+
+#6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#output: dict
+
+#7
+x = True
+print(type(x))
+#output: bool
+
